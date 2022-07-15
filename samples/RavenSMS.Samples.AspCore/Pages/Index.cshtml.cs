f072681f23f3c58c521a38d6b647afc9b32d57f7
Youssef Sellami <contact@youssefsellami.com>
2022-07-15 23:16:53 +0100

    added the base project files

---
+++ samples/RavenSMS.Samples.AspCore/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMS.Net.AspCore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
